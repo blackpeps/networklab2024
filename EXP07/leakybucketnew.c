4c0ffd500b2ae2f7a3356eb250ec5a4fd897d17a
blackpeps <126700907+blackpeps@users.noreply.github.com>
2024-07-05 23:37:53 +0530

    Update leakybucketnew.c

--- EXP07/leakybucketnew.c
+++ EXP07/leakybucketnew.c
@@ -1,61 +1,35 @@
 #include <stdio.h>
-
 int min(int x, int y) {
-    if (x < y) {
-        return x;
-    } else {
-        return y;
-    }
+    return (x < y) ? x : y;
 }
-
 int main() {
-    int drop = 0, mini, nsec, cap, count = 0;
-    int i, inp[25], process;
-
-    printf("Enter the Bucket Size:\n");
+    int drop = 0, input, cap, count = 0, outgoing;
+    int i, incoming[25];
+    printf("Enter the Bucket Size: ");
     scanf("%d", &cap);
-    printf("Enter the Processing Rate:\n");
-    scanf("%d", &process);
-    printf("Enter The No. Of Seconds You Want To Stimulate:\n");
-    scanf("%d", &nsec);
-
-    for (i = 0; i < nsec; i++) {
-        printf("Enter the Size of the Packet Entering at %d sec:\n", i + 1);
-        scanf("%d", &inp[i]);
+    printf("Enter the Output Rate: ");
+    scanf("%d", &outgoing);
+    printf("Enter The No. Of Inputs: ");
+    scanf("%d", &input);
+    for (i = 0; i < input; i++) {
+        printf("Enter the [%d] Incoming Packet Size: ", i + 1);
+        scanf("%d", &incoming[i]);
     }
-
-    printf("\nSecond|Packet Recieved|Packet Sent|Packet Left|Packet Dropped|\n");
-    printf("-------------------------------------------------------\n");
-
-    for (i = 0; i < nsec; i++) {
-        count += inp[i];
+    printf("\nInput|Packet Received|Packet Sent|Packet Left|Packet Dropped|\n");
+    printf("-------------------------------------------------------------------\n");
+    for (i = 0; i < input; i++) {
+        count += incoming[i];
         if (count > cap) {
             drop = count - cap;
             count = cap;
         }
-        printf("%d", i + 1);
-        printf("\t%d", inp[i]);
-        mini = min(count, process);
-        printf("\t\t\t\t%d", mini);
-        count = count - mini;
-        printf("\t\t\t\t%d", count);
-        printf("\t\t\t\t%d\n", drop);
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, incoming[i], min(count, outgoing), count - min(count, outgoing), drop);
+        count -= min(count, outgoing);
         drop = 0;
     }
-
-    for (; count != 0; i++) {
-        if (count > cap) {
-            drop = count - cap;
-            count = cap;
-        }
-        printf("%d", i + 1);
-        printf("\t0");
-        mini = min(count, process);
-        printf("\t\t%d", mini);
-        count = count - mini;
-        printf("\t\t%d", count);
-        printf("\t\t%d\n", drop);
+    for (; count > 0; i++) {
+        printf("%d\t0\t\t%d\t\t%d\t\t0\n", i + 1, min(count, outgoing), count - min(count, outgoing));
+        count -= min(count, outgoing);
     }
-
     return 0;
 }
